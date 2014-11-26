9d59458cf504347151aefed1840a8811654d76be
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-26 15:34:02 +0200

    Added jumbotron to the main index (home page)

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.default')
 
 @section('content')
-
-    Welcome to larabooooook! :D
-    
+<div class="jumbotron">
+  <h1>Welcome to larabooooook! :D</h1>
+  <p>Welcome to the premier place to talk about what you want with othere. You can talk about anything here ! :)</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up!</a></p>
+</div>
 @stop
