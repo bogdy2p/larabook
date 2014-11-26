59fa1ad1a7b557c3af46f7fd03569dc7278de92f
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-26 13:21:33 +0200

    Created pagescontroller , created pages view folder and homepage view. modified routes and app&database config

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+
+
+
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    </body>
+</html>
