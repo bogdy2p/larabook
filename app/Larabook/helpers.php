2f7eabe63f68c8d1ec52701d5175e325b3405dd1
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-28 14:23:23 +0200

    Added larabook Helpers file. Add link to the file into composer json

---
+++ app/Larabook/helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+function gravatar_link($email){
+    $email = md5($email);
+    
+    return "//www.gravatar.com/avatar/{$email}?s=30";
+}
--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -23,24 +23,22 @@
             <ul class="nav navbar-nav navbar-right">   
                 @if($currentUser)
                 <li class="dropdown">
-                    
-                    
-                    
+
+
+
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <img class="nav-gravatar" src="//www.gravatar.com/avatar/{{ md5($currentUser->email) }}?s=30" alt="{{ $currentUser->username }}">    
+                        <img class="nav-gravatar" src=" {{ gravatar_link($currentUser->email) }}" alt="{{ $currentUser->username }}">    
                         {{ $currentUser->username }} <span class="caret"></span>
-                    
+
                     </a>
-                    
-                    
+
+
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="#">Action</a></li>
                         <li><a href="#">Another action</a></li>
                         <li><a href="#">Something else here</a></li>
                         <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">One more separated link</a></li>
+                        <li>{{ link_to_route('logout_path','Log Out') }} </li>
                     </ul>
                 </li>
                 @else
