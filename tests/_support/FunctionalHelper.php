049ccd61231867fa9c6d882d4375de0b2708c384
Popa Bogdan <bogdy2p@yahoo.com>
2014-11-28 20:53:29 +0200

    Extracted idividual status view to it's own file (a partial one)

--- tests/_support/FunctionalHelper.php
+++ tests/_support/FunctionalHelper.php
@@ -33,7 +33,7 @@ class FunctionalHelper extends \Codeception\Module {
        // $this->have('Larabook\Statuses\Status', $overrides);
         
         $I = $this->getModule('Laravel4');
-        $I->fillField('Status:',$body);
+        $I->fillField('body',$body);
         $I->click('Post Status');
     }
 
