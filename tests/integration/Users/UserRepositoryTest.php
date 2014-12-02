2cb430951f26e17533659ab20e9f060679cd5a47
Popa Bogdan <bogdy2p@yahoo.com>
2014-12-02 11:17:46 +0200

    made test more readable

--- tests/integration/Users/UserRepositoryTest.php
+++ tests/integration/Users/UserRepositoryTest.php
@@ -42,31 +42,31 @@ class UserRepositoryTest extends \Codeception\TestCase\Test {
     /** @test */
     public function it_follows_another_user() {
         //given we have two users
-        $users = TestDummy::times(2)->create('Larabook\Users\User'); // [$user, $user]
+        list($vasile,$gheorghe) = TestDummy::times(2)->create('Larabook\Users\User'); // [$user, $user]
         // and one user follows another user
-        $this->repo->follow($users[1]->id, $users[0]);
+        $this->repo->follow($vasile->id, $gheorghe);
 
         //then i should see that user in the list of those that user0 follows.
         $this->tester->seeRecord('follows', [
-            'follower_id' => $users[0]->id,
-            'followed_id' => $users[1]->id
+            'follower_id' => $gheorghe->id,
+            'followed_id' => $vasile->id
         ]);
     }
 
         /** @test */
     public function it_unfollows_another_user() {
         //given we have two users
-        $users = TestDummy::times(2)->create('Larabook\Users\User'); // [$user, $user]
+        list($vasile,$gheorghe) = TestDummy::times(2)->create('Larabook\Users\User'); // [$user, $user]
         // and one user follows another user
-        $this->repo->follow($users[1]->id, $users[0]);
+        $this->repo->follow($vasile->id, $gheorghe);
         
         //when i unfollow that same user
-        $this->repo->unfollow($users[1]->id, $users[0]);
+        $this->repo->unfollow($vasile->id, $gheorghe);
 
         //then i should NOT see that user in the list of those that user0 follows.
         $this->tester->dontSeeRecord('follows', [
-            'follower_id' => $users[0]->id,
-            'followed_id' => $users[1]->id
+            'follower_id' => $gheorghe->id,
+            'followed_id' => $vasile->id
         ]);
     }
 
